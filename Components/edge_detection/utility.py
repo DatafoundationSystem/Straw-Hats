894dfb9ba69df92cdbdec98aeafdfebaf38bbffd
nileshjha4 <nileshjha412@gmail.com>
2023-03-19 16:29:47 +0530

    update component

--- Components/edge_detection/utility.py
+++
@@ -1,6 +0,0 @@
-def preprocess(image_url):
-    from src.src import simple_edge_detection, cv2
-    image = cv2.imread(image_url, 0)
-    img = simple_edge_detection(image)
-    cv2.imwrite('edge_detected.png', img) 
-    
