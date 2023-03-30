adfe9b22a6b8dbc1fb864f2f246e534213712108
Jasika16 <jasikashah1610@gmail.com>
2023-03-30 16:44:39 +0530

    Added Changes

--- Front_end/website/assets/js/custom.js
+++ Front_end/website/assets/js/custom.js
@@ -179,3 +179,33 @@ function printObject() {
   hide_text.style.display='none';
 
 }
+
+
+// -------------------------File Upload
+document.getElementById("uploadButton").onclick = () => {
+  let fileElement = document.getElementById('fileInput')
+
+  // check if user had selected a file
+  if (fileElement.files.length === 0) {
+    alert('please choose a file')
+    return
+  }
+
+  let file = fileElement.files[0]
+
+  let formData = new FormData();
+  formData.set('file', file);
+
+  axios.post("http://localhost:3001/upload-single-file", formData, {
+    onUploadProgress: progressEvent => {
+      const percentCompleted = Math.round(
+        (progressEvent.loaded * 100) / progressEvent.total
+      );
+      console.log(`upload process: ${percentCompleted}%`);
+    }
+  })
+    .then(res => {
+      console.log(res.data)
+      console.log(res.data.url)
+    })
+}
