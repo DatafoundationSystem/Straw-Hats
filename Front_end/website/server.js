4023605a81e54256ff6dbb60860801391b3bea67
nileshjha4 <nileshjha412@gmail.com>
2023-03-30 17:15:40 +0530

    update frontend

--- Front_end/website/server.js
+++ Front_end/website/server.js
@@ -1,18 +1,75 @@
-const path = require("path");
-const Koa = require("koa");
-const Router = require("@koa/router");
+const express = require("express");
+const path = require("path")
+const multer = require("multer")
+const app = express();
 
-const app = new Koa();
-const router = new Router();
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs")
 
-const PORT = 3000;
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
 
-router.get("/", async (ctx) => {
-  ctx.body = "Hello friends!";
+      // Uploads is the Upload_folder_name
+      cb(null, "uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now()+".jpg")
+  }
+})
+     
+const maxSize = 100 * 1000 * 1000;
+
+var upload = multer({ 
+  storage: storage,
+  limits: { fileSize: maxSize },
+  fileFilter: function (req, file, cb){
+  
+      // Set the filetypes, it is optional
+      var filetypes = /jpeg|jpg|png/;
+      var mimetype = filetypes.test(file.mimetype);
+
+      var extname = filetypes.test(path.extname(
+                  file.originalname).toLowerCase());
+      
+      if (mimetype && extname) {
+          return cb(null, true);
+      }
+    
+      cb("Error: File upload only supports the "
+              + "following filetypes - " + filetypes);
+    } 
+
+// mypic is the name of file attribute
+}).single("mypic"); 
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
 });
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(express.static(__dirname));
 
-app.listen(PORT, () => {
-  console.log(`app starting at port ${PORT}`);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/upload",function (req, res, next) {
+        
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  upload(req,res,function(err) {
+
+      if(err) {
+
+          // ERROR occurred (here it can be occurred due
+          // to uploading image of size greater than
+          // 1MB or uploading different file type)
+          res.send(err)
+      }
+      else {
+
+          // SUCCESS, image successfully uploaded
+          res.send("Success, Image uploaded!")
+      }
+  })
+})
