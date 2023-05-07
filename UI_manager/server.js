497e2cb144ec0e17a93364db6c82fe38d3b91b5a
Vishal <vishmishra8199@gmail.com>
2023-05-07 20:18:02 +0530

    login, Logout, register added

--- UI_manager/server.js
+++ UI_manager/server.js
@@ -7,8 +7,9 @@ const mysql = require("mysql")
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
 const cookie_parser = require('cookie-parser')
+// const flash = require('connect-flash') 
+// const session = require('express-session');
 // const axios = require('axios')
-
 
 const app = express();
 var cors = require('cors');
@@ -114,6 +115,71 @@ app.post("/", function(req,res){
     }
   });
 })
+
+
+app.get('/signup', (req,res)=>{
+  res.render('signup.ejs', {msg: ""});
+});
+
+
+app.post("/signup", function(req,res){
+  username = req.body.user.name;
+  password = req.body.user.password;
+  role = req.body.user.role;
+  confirm_password = req.body.user.cpassword;
+  
+  console.log(req.body.user)
+
+  if(password!=confirm_password){
+    console.log(1)
+    res.render('signup.ejs', {msg: "Password Not matched"});
+    return;
+  }
+    let r=0;
+    if(role=="Admin")r=1;
+    else r=2;
+
+    let insert_query = 'INSERT INTO dfs.users (username,pass,role) VALUES (?,?,?)';
+    let query = mysql.format(insert_query, [username,password,r]);
+    console.log(query);
+
+    mysqlconnection.query(query, (err,data)=>{
+      if(err){
+        console.log(err);
+        res.render('signup.ejs', {msg: "User Name already exists. Try again !!"});
+      }
+
+      let select_query = 'SELECT * FROM ?? WHERE ?? = ?';
+      let query = mysql.format(select_query, ["dfs.users", "username", username]);
+
+      mysqlconnection.query(query, (err,data)=>{
+        if(err){
+          console.log(err);
+          return;
+        }
+
+      var token = jwt.sign({username : username, userid:data[0].user_id}, "SuperSecretKey", {expiresIn : 86400});
+        console.log(token);
+        
+        res
+        .status(200)
+        .cookie("access_token" , token, {
+          httpOnly:true,
+        })
+        .redirect('/home');
+      });
+      return;
+
+    });
+    
+})
+
+app.get('/logout', (req,res)=>{
+  res.clearCookie('access_token');
+  res.redirect("/")
+});
+
+
 
 app.get("/home", (req, res) => {
   console.log(req.cookies['access_token']);
@@ -430,7 +496,7 @@ app.get("/viewResult", (req, res) => {
 
           let local_path = '/public' + image_path;
           // download files from mino to local storage ===============================
-          minioClient.fGetObject( 'uploads/' , image_name, local_path, function(err) {
+          minioClient.fGetObject( 'uploads' , image_name, local_path, function(err) {
             if (err) {
                 return console.log(err);
             }
@@ -450,3 +516,7 @@ app.get("/viewResult", (req, res) => {
 
     });
 });
+
+
+
+
